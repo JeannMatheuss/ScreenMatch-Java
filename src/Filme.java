7b66ac4c2068fed3c692f9fe8f4b11dcf6ce0719
Jean <jeanmatheus0612@gmail.com>
2024-12-16 22:57:13 -0300

    Criando metodos em Java

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,22 @@ public class Filme {
     public String nome;
     public int anoDeLancamento;
     public boolean incluidoNoPlano;
-    public double avaliacao;
+    public double somaDasAvaliacoes;
     public int totalDeAvaliacoes;
     public int duracaoEmMinutos;
+
+    // criando um metodo
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
