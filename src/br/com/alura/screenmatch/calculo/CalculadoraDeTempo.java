5dea0079d18912fbcc39306c67f9145734d11453
Jean <jeanmatheus0612@gmail.com>
2024-12-22 14:08:21 -0300

    Entendendo polimorfismo

--- src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -1,7 +1,8 @@
 package br.com.alura.screenmatch.calculo;
 
-import br.com.alura.screenmatch.modelos.Filme;
-import br.com.alura.screenmatch.modelos.Serie;
+// import br.com.alura.screenmatch.modelos.Filme;
+// import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal;
@@ -10,11 +11,15 @@ public class CalculadoraDeTempo {
         return this.tempoTotal;
     }
 
-    public void inclui(Filme f) {
-        this.tempoTotal += f.getDuracaoEmMinutos();
-    }
+    // public void inclui(Filme f) {
+    //     this.tempoTotal += f.getDuracaoEmMinutos();
+    // }
 
-    public void inclui(Serie s) {
-        this.tempoTotal += s.getDuracaoEmMinutos();
+    // public void inclui(Serie s) {
+    //     this.tempoTotal += s.getDuracaoEmMinutos();
+    // }
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 }
