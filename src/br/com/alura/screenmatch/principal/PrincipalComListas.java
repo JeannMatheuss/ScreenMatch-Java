5429b3fc9ed4bd80c3638a4b99a2333a04aad244
Jean <jeanmatheus0612@gmail.com>
2024-12-31 12:11:53 -0300

    Modificando a lista para receber filmes e series

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -12,10 +13,11 @@ public class PrincipalComListas {
         Filme filmeDoJeanzada = new Filme("Homem-Aranha 2", 2004);
         Serie lost = new Serie("Lost", 2004);
 
-        ArrayList<Filme> listaAssistidos = new ArrayList<>();
+        ArrayList<Titulo> listaAssistidos = new ArrayList<>();
         listaAssistidos.add(meuFilme);
         listaAssistidos.add(outroFilme);
         listaAssistidos.add(filmeDoJeanzada);
+        listaAssistidos.add(lost);
 
     }
 }
