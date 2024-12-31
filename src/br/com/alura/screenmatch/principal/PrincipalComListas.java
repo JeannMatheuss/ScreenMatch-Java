317d46d5c08f521744be3e837f76f9756748305a
Jean <jeanmatheus0612@gmail.com>
2024-12-31 12:19:18 -0300

    Fazendo o casting do filme

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -9,9 +9,13 @@ import br.com.alura.screenmatch.modelos.Titulo;
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme meuFilme = new Filme("O Poderoso Chefão", 1972);
+        meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(8);
         Filme filmeDoJeanzada = new Filme("Homem-Aranha 2", 2004);
+        filmeDoJeanzada.avalia(10);
         Serie lost = new Serie("Lost", 2004);
+        //lost.avalia(9);
 
         ArrayList<Titulo> listaAssistidos = new ArrayList<>();
         listaAssistidos.add(meuFilme);
@@ -20,7 +24,9 @@ public class PrincipalComListas {
         listaAssistidos.add(lost);
 
         for (Titulo item : listaAssistidos) {
-            System.out.println(item);
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classificação: " + filme.getClassificacao());
         }
     }
 }
