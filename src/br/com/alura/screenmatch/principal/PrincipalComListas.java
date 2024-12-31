f2782ea92d80728bea46306672bcae8a079875b9
Jean <jeanmatheus0612@gmail.com>
2024-12-31 12:09:44 -0300

    Colocando os filmes e a serie no principal com lista, e criando a lista de assistidos

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -1,7 +1,21 @@
 package br.com.alura.screenmatch.principal;
 
+import java.util.ArrayList;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class PrincipalComListas {
     public static void main(String[] args) {
-        
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1972);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        Filme filmeDoJeanzada = new Filme("Homem-Aranha 2", 2004);
+        Serie lost = new Serie("Lost", 2004);
+
+        ArrayList<Filme> listaAssistidos = new ArrayList<>();
+        listaAssistidos.add(meuFilme);
+        listaAssistidos.add(outroFilme);
+        listaAssistidos.add(filmeDoJeanzada);
+
     }
 }
