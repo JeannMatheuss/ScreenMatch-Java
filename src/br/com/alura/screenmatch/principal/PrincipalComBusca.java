ee96875dcdda0dd79e1a6bcc1993b433eb91f194
Jean <jeanmatheus0612@gmail.com>
2025-01-08 14:30:31 -0300

    Criando um script para buscar o filme que quiser

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,12 +6,20 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para a busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=3493ff3d";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=3493ff3d"))
+            .uri(URI.create(endereco))
             .build();
 
         HttpResponse<String> response = client
