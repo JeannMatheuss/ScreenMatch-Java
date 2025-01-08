ffd265d66cd22f0fffdbe47db0ba60edb0aa3e43
Jean <jeanmatheus0612@gmail.com>
2025-01-08 14:16:29 -0300

    Trazendo o httoResponse

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,14 +1,20 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=3493ff3d"))
             .build();
+
+        HttpResponse<String> response = client
+            .send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
