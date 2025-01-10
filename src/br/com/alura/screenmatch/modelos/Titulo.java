a67dfad4c1861cf3a3d744baa2e92314bb5904a2
Jean <jeanmatheus0612@gmail.com>
2025-01-10 15:02:45 -0300

    Usando o toString para a busca do nome e anoDeLancamento

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -82,4 +82,12 @@ public class Titulo implements Comparable<Titulo> {
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
+
+
+    @Override
+    public String toString() {
+        return "Titulo [nome=" + nome + ", anoDeLancamento=" + anoDeLancamento + "]";
+    }
+
+    
 }
